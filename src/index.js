e5157ad9c33720019aaa423d95e142e9a76d8e13
Stanislau-Tuchto <toshiro_mifune@mail.ru>
2018-02-25 02:18:10 +0300

    impement makeExchange function 16ms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let obj={},H,Q,D,N;
+    if(currency <= 10000 && currency > 0){
+        obj.H = div(currency, 50);
+        H = currency%50;
+        obj.Q = div(H, 25);
+        Q = H%25;
+        obj.D = div(Q, 10);
+        D = Q%10;
+        obj.N = div(D, 5);
+        N = D%5;
+        obj.P = div(N, 1);
+        console.log(currency);
+        console.log(obj);
+        for(var key in obj) {
+            if (obj[key] === 0) {
+                delete obj[key];
+            }
+        }
+        console.log(obj);
+        return obj;
+    }else if(currency <= 0){
+        return obj;
+    } else
+        return {error:"You are rich, my friend! We don't have so much coins for exchange"};
 }
+function div(val, by){
+    return (val - val % by) / by;
+}
+
